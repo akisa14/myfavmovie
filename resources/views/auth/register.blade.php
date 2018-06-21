04e727ee0ca11638a4646aeadaf8f11e26d985db
akisa14 <akisyg921@gmail.com>
2018-06-21 05:32:09 +0000

    updated for now

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,7 +23,7 @@
                     </div>
 
                     <div class="form-group">
-                        {!! Form::label('password_confirmation', 'Password（Confirmation）') !!}
+                        {!! Form::label('password_confirmation', 'Password（Confirm）') !!}
                         {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                     </div>
 
