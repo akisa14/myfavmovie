9949628034b77315016c4d1a80ecffb0ab8778a0
akisa14 <akisyg921@gmail.com>
2018-06-21 01:28:20 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-xs-offset-3 col-xs-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">Registraion</div>
+            <div class="panel-body">
+                {!! Form::open(['route' => 'signup.post']) !!}
+                    <div class="form-group">
+                        {!! Form::label('name', 'name') !!}
+                        {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('email', 'email') !!}
+                        {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password', 'Password') !!}
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password_confirmation', 'Password（Confirmation）') !!}
+                        {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="text-right">
+                        {!! Form::submit('Signup', ['class' => 'btn btn-success']) !!}
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
